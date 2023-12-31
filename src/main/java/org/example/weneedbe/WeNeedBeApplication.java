5a7d10929958a03509bcfb5e99e302ba714c5199
노정완 <jeongwan.noh@gmail.com>
2023-12-31 21:03:25 +0900

    :tada:[feat]: Initial Commit

---
+++ src/main/java/org/example/weneedbe/WeNeedBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.weneedbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeNeedBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeNeedBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/weneedbe/WeNeedBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.weneedbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeNeedBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
