4fced035bef0a7723e0e3d4ddefb7de4fb617884
jiixon <jr0107380@gmail.com>
2024-01-02 18:35:24 +0900

    :sparkles:[feat]: BaseTimeEntity 설정

--- src/main/java/org/example/weneedbe/WeNeedBeApplication.java
+++ src/main/java/org/example/weneedbe/WeNeedBeApplication.java
@@ -2,7 +2,9 @@ package org.example.weneedbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class WeNeedBeApplication {
 
