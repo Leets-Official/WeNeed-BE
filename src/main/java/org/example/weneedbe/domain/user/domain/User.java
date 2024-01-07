1e66969fd0aecc8f4deece44009bc6986d04b1ae
jiixon <jr0107380@gmail.com>
2024-01-07 19:57:24 +0900

    :sparkles:[feat]: User Entity 생성

---
+++ src/main/java/org/example/weneedbe/domain/user/domain/User.java
@@ -0,0 +1,58 @@
+package org.example.weneedbe.domain.user.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.example.weneedbe.global.shared.entity.BaseTimeEntity;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name="users")
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "major", nullable = true)
+    private Department major;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "double_major", nullable = true)
+    private Department doubleMajor;
+
+    @Column(name = "user_nickname", nullable = true)
+    private String nickname;
+
+    @Column(name = "user_grade", nullable = true)
+    private Integer grade;
+
+    @Column(name = "user_age", nullable = true)
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "interest_field", nullable = true)
+    private Interests interestField;
+
+    @ElementCollection
+    @CollectionTable(name = "user_links", joinColumns = @JoinColumn(name = "user_id"))
+    private List<String> link;
+
+    @Column(name = "has_registered", nullable = false)
+    private LocalDateTime hasRegistered;
+
+    @Column(name = "deleted_at", nullable = true)
+    private LocalDateTime deletedAt;
+
+}
