7e5c08fe7e6cfe213bc822e87d4a4e9fd0fe4828
anxi01 <tjdals2893@naver.com>
2024-01-09 03:06:19 +0900

    :sparkles: [feat]: User Entity Getter 추가

--- src/main/java/org/example/weneedbe/domain/user/domain/User.java
+++ src/main/java/org/example/weneedbe/domain/user/domain/User.java
@@ -3,6 +3,7 @@ package org.example.weneedbe.domain.user.domain;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.weneedbe.domain.bookmark.domain.Bookmark;
 import org.example.weneedbe.global.shared.entity.BaseTimeEntity;
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
