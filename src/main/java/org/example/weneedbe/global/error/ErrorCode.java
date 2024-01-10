6c72604724c4ca0d5e1da4e4c0f43c9c75fc4d71
anxi01 <tjdals2893@naver.com>
2024-01-11 01:46:54 +0900

    :sparkles: [feat]: 유저 관련 예외 처리 추가

--- src/main/java/org/example/weneedbe/global/error/ErrorCode.java
+++ src/main/java/org/example/weneedbe/global/error/ErrorCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR", "서버 오류가 발생했습니다.");
+    INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR", "서버 오류가 발생했습니다."),
+    USER_NOT_FOUND(400, "USER_NOT_FOUND", "존재하지 않는 사용자입니다.");
 
     private final int httpStatus;
     private final String code;
