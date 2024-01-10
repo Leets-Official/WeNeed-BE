0a2ca4311316d1f484fc8b923e357272586a8673
노정완 <jeongwan.noh@gmail.com>
2024-01-10 14:46:00 +0900

    :sparkles: [feat]: RefreshToken 도메인 생성

--- src/main/java/org/example/weneedbe/domain/user/domain/User.java
+++ src/main/java/org/example/weneedbe/domain/user/domain/User.java
@@ -3,6 +3,7 @@ package org.example.weneedbe.domain.user.domain;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.weneedbe.domain.bookmark.domain.Bookmark;
 import org.example.weneedbe.global.shared.entity.BaseTimeEntity;
@@ -13,6 +14,7 @@ import java.util.List;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 @Builder
 @Table(name="users")
 public class User extends BaseTimeEntity {
