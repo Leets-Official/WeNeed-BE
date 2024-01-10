b30176755c714753fa475738dbe6396ae9d6d1f0
노정완 <jeongwan.noh@gmail.com>
2024-01-10 16:11:51 +0900

    :sparkles: [feat]: RefreshToken 서비스 로직 구현

---
+++ src/main/java/org/example/weneedbe/global/config/jwt/service/RefreshTokenService.java
@@ -0,0 +1,17 @@
+package org.example.weneedbe.global.config.jwt.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.weneedbe.domain.token.domain.RefreshToken;
+import org.example.weneedbe.domain.token.repository.RefreshTokenRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(IllegalArgumentException::new);
+    }
+}
