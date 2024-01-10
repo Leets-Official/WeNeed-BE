0058337017fbe8dd07281b77b8a808cf8f230d3e
노정완 <jeongwan.noh@gmail.com>
2024-01-10 13:50:25 +0900

    :sparkles: [feat]: 시큐리티 설정 변경

--- src/main/java/org/example/weneedbe/global/config/security/SecurityConfig.java
+++ src/main/java/org/example/weneedbe/global/config/security/SecurityConfig.java
@@ -1,21 +1,59 @@
 package org.example.weneedbe.global.config.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
-        .authorizeHttpRequests(
-            request -> request.requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll());
+            .httpBasic(httpBasic ->
+                    httpBasic.disable()
+            )
+            .cors(corsConfig ->
+                    corsConfig.configurationSource(corsConfigurationSource()))
+            .csrf(csrf ->
+                    csrf.disable()
+            )
+            .formLogin(formLogin ->
+                    formLogin.disable())
+            .sessionManagement((sessionManagement) ->
+                    sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .authorizeHttpRequests((authorize) ->
+                    authorize
+                            .requestMatchers("/").permitAll()
+                            .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                            .anyRequest().permitAll()
+            );
 
     return http.build();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("*"));
+    configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+    configuration.setAllowCredentials(true);
+    configuration.setAllowedHeaders(Arrays.asList("*"));
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 }
