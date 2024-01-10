6b248527553ae0693417019cac3fc16c4b0a6dd3
anxi01 <tjdals2893@naver.com>
2024-01-11 01:35:10 +0900

    :recycle: [refactor]: 게시글 작성 controller 매개변수 변경

--- src/main/java/org/example/weneedbe/domain/article/api/ArticleController.java
+++ src/main/java/org/example/weneedbe/domain/article/api/ArticleController.java
@@ -1,5 +1,6 @@
 package org.example.weneedbe.domain.article.api;
 
+import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.weneedbe.domain.article.application.ArticleService;
@@ -9,9 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,14 +22,21 @@ public class ArticleController {
   private final ArticleService articleService;
 
   @PostMapping("/portfolio")
-  public ResponseEntity<Void> createPortfolio(@RequestBody AddArticleRequest request) {
-    articleService.createPortfolio(request);
+  public ResponseEntity<Void> createPortfolio(@RequestPart MultipartFile thumbnail,
+      @RequestPart List<MultipartFile> images,
+      @RequestPart List<MultipartFile> files, @RequestPart AddArticleRequest request) throws IOException {
+
+    articleService.createPortfolio(thumbnail, images, files, request);
     return ResponseEntity.status(HttpStatus.CREATED).build();
   }
 
-  @PostMapping("/recruit")
-  public ResponseEntity<Void> createRecruit(@RequestBody AddArticleRequest request) {
-    articleService.createRecruit(request);
+    @PostMapping("/recruit")
+  public ResponseEntity<Void> createRecruit(@RequestPart MultipartFile thumbnail,
+        @RequestPart List<MultipartFile> images,
+        @RequestPart List<MultipartFile> files, @RequestPart AddArticleRequest request)
+        throws IOException {
+
+    articleService.createRecruit(thumbnail, images, files, request);
     return ResponseEntity.status(HttpStatus.CREATED).build();
   }
 
