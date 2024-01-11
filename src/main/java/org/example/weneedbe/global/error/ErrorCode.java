4fc66e33d3392d1937649625885dc811956127e2
anxi01 <tjdals2893@naver.com>
2024-01-11 16:32:06 +0900

    :sparkles: [feat]: ArticleNotFound 예외 설정 추가

--- src/main/java/org/example/weneedbe/global/error/ErrorCode.java
+++ src/main/java/org/example/weneedbe/global/error/ErrorCode.java
@@ -9,7 +9,8 @@ public enum ErrorCode {
     INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR", "서버 오류가 발생했습니다."),
     USER_NOT_FOUND(400, "USER_NOT_FOUND", "존재하지 않는 사용자입니다."),
     INVALID_IMAGE_FILE(400, "INVALID_IMAGE_FILE", "PNG, JPEG, JPG 이미지 파일만 가능합니다."),
-    FILE_UPLOAD_ERROR(500, "FILE_UPLOAD_ERROR", "파일 업로드에 실패했습니다.");
+    FILE_UPLOAD_ERROR(500, "FILE_UPLOAD_ERROR", "파일 업로드에 실패했습니다."),
+    ARTICLE_NOT_FOUND(400, "ARTICLE_NOT_FOUND", "존재하지 않는 게시물입니다.");
 
     private final int httpStatus;
     private final String code;
