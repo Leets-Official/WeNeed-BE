ec6525989b27a24e7b22800e949dbfe3372af69b
anxi01 <tjdals2893@naver.com>
2024-01-11 16:31:20 +0900

    :sparkles: [feat]: 좋아요, 북마크 관련 repository 생성

---
+++ src/main/java/org/example/weneedbe/domain/article/repository/ArticleLikeRepository.java
@@ -0,0 +1,12 @@
+package org.example.weneedbe.domain.article.repository;
+
+import java.util.Optional;
+import org.example.weneedbe.domain.article.domain.Article;
+import org.example.weneedbe.domain.article.domain.ArticleLike;
+import org.example.weneedbe.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long> {
+
+  Optional<ArticleLike> findByArticleAndUser(Article article, User user);
+}
---
+++ src/main/java/org/example/weneedbe/domain/bookmark/repository/BookmarkRepository.java
@@ -0,0 +1,12 @@
+package org.example.weneedbe.domain.bookmark.repository;
+
+import java.util.Optional;
+import org.example.weneedbe.domain.article.domain.Article;
+import org.example.weneedbe.domain.bookmark.domain.Bookmark;
+import org.example.weneedbe.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+  Optional<Bookmark> findByArticleAndUser(Article article, User user);
+}
