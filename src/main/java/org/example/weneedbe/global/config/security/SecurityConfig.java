9961839bfd01eeaa61366948be71fd096f0cb5d6
노정완 <jeongwan.noh@gmail.com>
2024-01-12 00:19:19 +0900

    :sparkles: [feat]: SecurityConfig 내 토큰 필터 적용

--- src/main/java/org/example/weneedbe/global/config/security/SecurityConfig.java
+++ src/main/java/org/example/weneedbe/global/config/security/SecurityConfig.java
@@ -1,13 +1,15 @@
 package org.example.weneedbe.global.config.security;
 
 import lombok.RequiredArgsConstructor;
+import org.example.weneedbe.global.config.jwt.TokenAuthenticationFilter;
+import org.example.weneedbe.global.config.jwt.TokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -18,6 +20,7 @@ import java.util.Arrays;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final TokenProvider tokenProvider;
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -35,6 +38,8 @@ public class SecurityConfig {
             .sessionManagement((sessionManagement) ->
                     sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
+            .addFilterBefore(new TokenAuthenticationFilter(tokenProvider),
+                    UsernamePasswordAuthenticationFilter.class)
             .authorizeHttpRequests((authorize) ->
                     authorize
                             .requestMatchers("/").permitAll()
