7e26e7e81a9cfec7ffe062c2f6b92e9dff6a86a0
노정완 <jeongwan.noh@gmail.com>
2024-01-13 01:46:53 +0900

    :sparkles: [feat]: 쿠키 관리 클래스 생성
    쿠카 추가 및 만료 처리 로직, 직렬화 및 역직렬화 로직

---
+++ src/main/java/org/example/weneedbe/global/util/CookieUtil.java
@@ -0,0 +1,48 @@
+package org.example.weneedbe.global.util;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.SerializationUtils;
+
+import java.util.Base64;
+
+public class CookieUtil {
+    // 요청값을 바탕으로 한 쿠키 추가
+    public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setMaxAge(maxAge);
+        response.addCookie(cookie);
+    }
+
+    //쿠키의 이름값을 토대로 한 쿠키 삭제
+    public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            return;
+        }
+
+        for (Cookie cookie : cookies) {
+            if (name.equals(cookie.getName())) {
+                cookie.setValue("");
+                cookie.setPath("/");
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+            }
+        }
+    }
+
+    public static String serialize(Object object) {
+        return Base64.getUrlEncoder()
+                .encodeToString(SerializationUtils.serialize(object));
+    }
+
+    public static <T> T deserialize(Cookie cookie, Class<T> tClass) {
+        return tClass.cast(
+                SerializationUtils.deserialize(
+                        Base64.getUrlDecoder().decode(cookie.getValue())
+                )
+        );
+    }
+}
