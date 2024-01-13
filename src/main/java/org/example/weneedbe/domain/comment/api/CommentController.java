90af58f8c028cf7ab9f042c15a0d41f7f4afb0fd
anxi01 <tjdals2893@naver.com>
2024-01-13 20:37:17 +0900

    :memo: [docs]: 댓글 controller swagger 문서화

--- src/main/java/org/example/weneedbe/domain/comment/api/CommentController.java
+++ src/main/java/org/example/weneedbe/domain/comment/api/CommentController.java
@@ -1,8 +1,14 @@
 package org.example.weneedbe.domain.comment.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.example.weneedbe.domain.comment.application.CommentService;
 import org.example.weneedbe.domain.comment.dto.request.AddCommentRequest;
+import org.example.weneedbe.global.error.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,6 +25,13 @@ public class CommentController {
 
   private final CommentService commentService;
 
+  @Operation(summary = "댓글 작성", description = "리크루팅 게시물에 댓글을 작성합니다.")
+  @ApiResponses({
+      @ApiResponse(responseCode = "201"),
+      @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "401", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+  })
   @PostMapping("/{articleId}")
   public ResponseEntity<Void> createComment(@PathVariable Long articleId,
       @RequestBody AddCommentRequest request) {
@@ -26,6 +39,13 @@ public class CommentController {
     return ResponseEntity.status(HttpStatus.CREATED).build();
   }
 
+  @Operation(summary = "댓글 삭제", description = "사용자가 댓글을 삭제합니다.")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200"),
+      @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "401", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+  })
   @DeleteMapping("/{articleId}/{commentId}")
   public ResponseEntity<Void> deleteComment(@PathVariable Long articleId,
       @PathVariable Long commentId) {
