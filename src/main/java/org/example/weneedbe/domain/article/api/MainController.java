b63b17d5c15c568185c73e53c2672a80262e1e0d
jiixon <jr0107380@gmail.com>
2024-01-14 05:10:35 +0900

    :sparkles:[feat]: getMainPagePortfolio controller 구현

--- src/main/java/org/example/weneedbe/domain/article/api/MainController.java
+++ src/main/java/org/example/weneedbe/domain/article/api/MainController.java
@@ -7,12 +7,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.example.weneedbe.domain.article.application.MainService;
 import org.example.weneedbe.domain.article.dto.response.main.MainPortfolioDto;
 import org.example.weneedbe.global.error.ErrorResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +19,8 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class MainController {
+    private final MainService mainService;
+
 
     @Operation(summary = "메인페이지 - 포트폴리오", description = "메인페이지에서 포트폴리오부분 조회합니다")
     @ApiResponses({
@@ -28,10 +29,9 @@ public class MainController {
             @ApiResponse(responseCode = "401", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
             @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
-    @GetMapping("/portfolio")
+    @GetMapping("/portfolio}")
     ResponseEntity<MainPortfolioDto> getMainPagePortfolio(
-            @RequestParam int size, @RequestParam int page, @RequestParam String sort, @RequestParam List<String> category){
-
-
+            @RequestParam int size, @RequestParam int page, @RequestParam String sort, @RequestParam List<String> detailTags) {
+        return ResponseEntity.ok(mainService.getMainArticleList(size, page, sort, detailTags));
     }
 }
