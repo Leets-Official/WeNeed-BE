fd0006fd899074815c9401504a58456e118e2b7d
jiixon <jr0107380@gmail.com>
2024-01-14 05:06:53 +0900

    :sparkles:[feat]: InvalidSortException 클래스 생성

--- src/main/java/org/example/weneedbe/domain/article/exception/InvalidSortException.java
+++ src/main/java/org/example/weneedbe/domain/article/exception/InvalidSortException.java
@@ -1,2 +1,10 @@
-package org.example.weneedbe.domain.article.exception;public class InvalidSortException {
+package org.example.weneedbe.domain.article.exception;
+
+import org.example.weneedbe.global.error.ErrorCode;
+import org.example.weneedbe.global.error.exception.ServiceException;
+
+public class InvalidSortException extends ServiceException {
+    public InvalidSortException() {
+        super(ErrorCode.ARTICLE_NOT_FOUND);
+    }
 }
