c22d415955a55fb416d083ae2e40eaba2da47bdb
노정완 <jeongwan.noh@gmail.com>
2024-01-15 14:45:48 +0900

    :fire: [fix]: 로그인 페이지 매핑 임시 삭제

--- src/main/java/org/example/weneedbe/global/config/security/SecurityConfig.java
+++ src/main/java/org/example/weneedbe/global/config/security/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
@@ -60,7 +59,7 @@ public class SecurityConfig {
                       UsernamePasswordAuthenticationFilter.class)
 
               .oauth2Login(oauth2Login ->
-                      oauth2Login.loginPage("/login")
+                      oauth2Login
                               .authorizationEndpoint(authorization -> authorization
                                       .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository()))
                               .successHandler(oAuth2SuccessHandler())
