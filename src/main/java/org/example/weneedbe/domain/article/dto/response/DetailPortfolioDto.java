a2f5b3bdd54aae12c33a109e1bb8dcda58d74e2f
jiixon <jr0107380@gmail.com>
2024-01-18 23:45:37 +0900

    :sparkles:[feat]: detailPortfolioDto 생성 및 기능구현

--- src/main/java/org/example/weneedbe/domain/article/dto/response/DetailPortfolioDto.java
+++ src/main/java/org/example/weneedbe/domain/article/dto/response/DetailPortfolioDto.java
@@ -1,10 +1,13 @@
 package org.example.weneedbe.domain.article.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.*;
+import org.example.weneedbe.domain.article.domain.Article;
+import org.example.weneedbe.domain.article.domain.ContentData;
 import org.example.weneedbe.domain.user.domain.Department;
+import org.example.weneedbe.domain.user.domain.User;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -14,15 +17,25 @@ public class DetailPortfolioDto {
     private DetailPortfolioArticleDto portfolio;
     private List<WorkPortfolioArticleDto> workList;
 
-    @Getter
-    public static class DetailUserDto {
-        private String nickname;
-        private boolean sameUser;
+    public DetailPortfolioDto(Article article, User user, int heartCount, int bookmarkCount, List<Article> userPortfolio) {
+        this.user = new DetailUserDto(user, user.getUserId().equals(article.getUser().getUserId()));
+        this.portfolio = new DetailPortfolioArticleDto(article, heartCount, bookmarkCount);
+        this.workList = initializeWorkList(userPortfolio, article);
     }
 
+    @Getter
+    public class DetailUserDto {
+        private String nickname;
+        private boolean sameUser;
+
+        public DetailUserDto(User user, boolean sameUser) {
+            this.nickname = user.getNickname();
+            this.sameUser = sameUser;
+        }
+    }
 
     @Getter
-    public static class DetailPortfolioArticleDto {
+    public class DetailPortfolioArticleDto {
         private String thumbnail;
         private String title;
         private LocalDateTime createdAt;
@@ -32,19 +45,55 @@ public class DetailPortfolioDto {
         private DetailWriterPortfolioDto writer;
         private List<DetailPortfolioContentDto> contents;
 
+        public DetailPortfolioArticleDto(Article article, int heatCount, int bookmarkCount) {
+            this.thumbnail = article.getThumbnail();
+            this.title = article.getTitle();
+            this.createdAt = article.getCreatedAt();
+            this.heatCount = heatCount;
+            this.viewCount = article.getViewCount();
+            this.bookmarkCount = bookmarkCount;
+            this.writer = new DetailWriterPortfolioDto(article);
+            this.contents = getPortfolioContents(article.getContent());
+        }
+
+    }
+
+    private List<DetailPortfolioContentDto> getPortfolioContents(List<ContentData> contents) {
+        List<DetailPortfolioContentDto> contentDtoList = new ArrayList<>();
+        for (ContentData contentData : contents) {
+            DetailPortfolioContentDto contentDto = new DetailPortfolioContentDto();
+            contentDto.setId(contentData.getId());
+            contentDto.setType(contentData.getType());
+            if ("image".equals(contentData.getType())) {
+                contentDto.setImageData(contentData.getImageData());
+            } else if ("text".equals(contentData.getType())) {
+                contentDto.setTextData(contentData.getTextData());
+            }
+            contentDtoList.add(contentDto);
+        }
+        return contentDtoList;
     }
 
     @Getter
-    public static class DetailWriterPortfolioDto {
+    public class DetailWriterPortfolioDto {
         private Long userId;
         private String writerNickname;
         private Department major;
         private String profile;
         private Integer grade;
+
+        public DetailWriterPortfolioDto(Article article) {
+            this.userId = article.getUser().getUserId();
+            this.writerNickname = article.getUser().getNickname();
+            this.major = article.getUser().getMajor();
+            this.profile = article.getUser().getProfile();
+            this.grade = article.getUser().getGrade();
+        }
     }
 
     @Getter
-    public static class DetailPortfolioContentDto{
+    @Setter
+    public class DetailPortfolioContentDto {
         private Long id;
         private String type;
         private String textData;
@@ -52,13 +101,26 @@ public class DetailPortfolioDto {
     }
 
     @Getter
-    public static class WorkPortfolioArticleDto{
+    public class WorkPortfolioArticleDto {
         private Long articleId;
         private String thumbnail;
         private String title;
+
+        public WorkPortfolioArticleDto(Article article) {
+            this.articleId = article.getArticleId();
+            this.thumbnail = article.getThumbnail();
+            this.title = article.getTitle();
+        }
     }
 
-
-
-
+    private List<WorkPortfolioArticleDto> initializeWorkList(List<Article> userPortfolio, Article article) {
+        List<WorkPortfolioArticleDto> workList = new ArrayList<>();
+        for (Article portfolio : userPortfolio) {
+            /* 상세조회하는 게시물 제외하고 workList에 추가 */
+            if (!portfolio.getArticleId().equals(article.getArticleId())) {
+                workList.add(new WorkPortfolioArticleDto(portfolio));
+            }
+        }
+        return workList;
+    }
 }
