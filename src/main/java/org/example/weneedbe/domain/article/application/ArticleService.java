c43528d9173ae471963ac6f50254dc8164603924
jiixon <jr0107380@gmail.com>
2024-01-18 23:46:21 +0900

    :sparkles:[feat]: getDetailPortfolio 기능 구현

--- src/main/java/org/example/weneedbe/domain/article/application/ArticleService.java
+++ src/main/java/org/example/weneedbe/domain/article/application/ArticleService.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
 import org.example.weneedbe.domain.article.domain.Article;
 import org.example.weneedbe.domain.article.domain.ArticleLike;
 import org.example.weneedbe.domain.article.dto.request.AddArticleRequest;
+import org.example.weneedbe.domain.article.dto.response.DetailPortfolioDto;
 import org.example.weneedbe.domain.article.dto.response.MemberInfoResponse;
 import org.example.weneedbe.domain.article.exception.ArticleNotFoundException;
 import org.example.weneedbe.domain.article.repository.ArticleLikeRepository;
@@ -26,92 +28,108 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class ArticleService {
 
-  private final ArticleRepository articleRepository;
-  private final UserRepository userRepository;
-  private final S3Service s3Service;
-  private final ArticleLikeRepository articleLikeRepository;
-  private final BookmarkRepository bookmarkRepository;
+    private final ArticleRepository articleRepository;
+    private final UserRepository userRepository;
+    private final S3Service s3Service;
+    private final ArticleLikeRepository articleLikeRepository;
+    private final BookmarkRepository bookmarkRepository;
 
-  public void createPortfolio(MultipartFile thumbnail, List<MultipartFile> images,
-      List<MultipartFile> files, AddArticleRequest request) throws IOException {
+    public void createPortfolio(MultipartFile thumbnail, List<MultipartFile> images,
+                                List<MultipartFile> files, AddArticleRequest request) throws IOException {
 
-    String thumbnailUrl = s3Service.uploadImage(thumbnail);
-    List<String> imageUrls = s3Service.uploadImages(images);
-    List<String> fileUrls = s3Service.uploadFile(files);
+        String thumbnailUrl = s3Service.uploadImage(thumbnail);
+        List<String> imageUrls = s3Service.uploadImages(images);
+        List<String> fileUrls = s3Service.uploadFile(files);
 
-    /* 토큰을 통한 user 객체를 불러옴 */
-    /* 아직 토큰이 없기 때문에 임시 객체를 사용 */
-    User mockUser = userRepository.findById(1L).orElseThrow();
+        /* 토큰을 통한 user 객체를 불러옴 */
+        /* 아직 토큰이 없기 때문에 임시 객체를 사용 */
+        User mockUser = userRepository.findById(1L).orElseThrow();
 
-    Article article = Article.of(thumbnailUrl, imageUrls, fileUrls, request, mockUser);
+        Article article = Article.of(thumbnailUrl, imageUrls, fileUrls, request, mockUser);
 
-    List<UserArticle> userArticles = new ArrayList<>();
-    /* 개인 프로젝트일 경우, 본인만 추가한다. */
-    userArticles.add(new UserArticle(mockUser, article));
+        List<UserArticle> userArticles = new ArrayList<>();
+        /* 개인 프로젝트일 경우, 본인만 추가한다. */
+        userArticles.add(new UserArticle(mockUser, article));
 
-    /* 팀원 존재시 작성자를 제외한 팀원만 넣는다. */
-    if (!request.getTeamMembersId().isEmpty()) {
-      for (Long memberId : request.getTeamMembersId()) {
-        User user = userRepository.findById(memberId)
-            .orElseThrow(UserNotFoundException::new);
-        userArticles.add(new UserArticle(user, article));
-      }
+        /* 팀원 존재시 작성자를 제외한 팀원만 넣는다. */
+        if (!request.getTeamMembersId().isEmpty()) {
+            for (Long memberId : request.getTeamMembersId()) {
+                User user = userRepository.findById(memberId)
+                        .orElseThrow(UserNotFoundException::new);
+                userArticles.add(new UserArticle(user, article));
+            }
+        }
+        article.setUserArticles(userArticles);
+        articleRepository.save(article);
     }
-    article.setUserArticles(userArticles);
-    articleRepository.save(article);
-  }
 
-  public void createRecruit(MultipartFile thumbnail, List<MultipartFile> images,
-      List<MultipartFile> files, AddArticleRequest request) throws IOException {
+    public void createRecruit(MultipartFile thumbnail, List<MultipartFile> images,
+                              List<MultipartFile> files, AddArticleRequest request) throws IOException {
 
-    String thumbnailUrl = s3Service.uploadImage(thumbnail);
-    List<String> imageUrls = s3Service.uploadImages(images);
-    List<String> fileUrls = s3Service.uploadFile(files);
+        String thumbnailUrl = s3Service.uploadImage(thumbnail);
+        List<String> imageUrls = s3Service.uploadImages(images);
+        List<String> fileUrls = s3Service.uploadFile(files);
 
-    /* 토큰을 통한 user 객체를 불러옴 */
-    /* 아직 토큰이 없기 때문에 임시 객체를 사용 */
-    User mockUser = userRepository.findById(1L).orElseThrow();
+        /* 토큰을 통한 user 객체를 불러옴 */
+        /* 아직 토큰이 없기 때문에 임시 객체를 사용 */
+        User mockUser = userRepository.findById(1L).orElseThrow();
 
-    Article article = Article.of(thumbnailUrl, imageUrls, fileUrls, request, mockUser);
-    article.setUserArticles(List.of(new UserArticle(mockUser, article)));
+        Article article = Article.of(thumbnailUrl, imageUrls, fileUrls, request, mockUser);
+        article.setUserArticles(List.of(new UserArticle(mockUser, article)));
 
-    articleRepository.save(article);
-  }
-
-  public List<MemberInfoResponse> getMemberList(String nickname) {
-    return userRepository.findAllByNicknameStartingWith(nickname).stream()
-        .map(MemberInfoResponse::new).toList();
-  }
-
-  public void likeArticle(Long articleId) {
-    /* 토큰을 통한 user 객체를 불러옴 */
-    /* 아직 토큰이 없기 때문에 임시 객체를 사용 */
-    User mockUser = userRepository.findById(1L).orElseThrow(UserNotFoundException::new);
-    Article article = articleRepository.findById(articleId).orElseThrow(ArticleNotFoundException::new);
-
-    Optional<ArticleLike> articleLike = articleLikeRepository.findByArticleAndUser(article,
-        mockUser);
-
-    if (articleLike.isEmpty()) {
-      articleLikeRepository.save(new ArticleLike(mockUser, article));
-    } else {
-      articleLikeRepository.delete(articleLike.get());
+        articleRepository.save(article);
     }
-  }
 
-  public void bookmarkArticle(Long articleId) {
-    /* 토큰을 통한 user 객체를 불러옴 */
-    /* 아직 토큰이 없기 때문에 임시 객체를 사용 */
-    User mockUser = userRepository.findById(1L).orElseThrow(UserNotFoundException::new);
-    Article article = articleRepository.findById(articleId).orElseThrow(ArticleNotFoundException::new);
-
-    Optional<Bookmark> bookmark = bookmarkRepository.findByArticleAndUser(article,
-        mockUser);
-
-    if (bookmark.isEmpty()) {
-      bookmarkRepository.save(new Bookmark(mockUser, article));
-    } else {
-      bookmarkRepository.delete(bookmark.get());
+    public List<MemberInfoResponse> getMemberList(String nickname) {
+        return userRepository.findAllByNicknameStartingWith(nickname).stream()
+                .map(MemberInfoResponse::new).toList();
     }
-  }
+
+    public void likeArticle(Long articleId) {
+        /* 토큰을 통한 user 객체를 불러옴 */
+        /* 아직 토큰이 없기 때문에 임시 객체를 사용 */
+        User mockUser = userRepository.findById(1L).orElseThrow(UserNotFoundException::new);
+        Article article = articleRepository.findById(articleId).orElseThrow(ArticleNotFoundException::new);
+
+        Optional<ArticleLike> articleLike = articleLikeRepository.findByArticleAndUser(article,
+                mockUser);
+
+        if (articleLike.isEmpty()) {
+            articleLikeRepository.save(new ArticleLike(mockUser, article));
+        } else {
+            articleLikeRepository.delete(articleLike.get());
+        }
+    }
+
+    public void bookmarkArticle(Long articleId) {
+        /* 토큰을 통한 user 객체를 불러옴 */
+        /* 아직 토큰이 없기 때문에 임시 객체를 사용 */
+        User mockUser = userRepository.findById(1L).orElseThrow(UserNotFoundException::new);
+        Article article = articleRepository.findById(articleId).orElseThrow(ArticleNotFoundException::new);
+
+        Optional<Bookmark> bookmark = bookmarkRepository.findByArticleAndUser(article,
+                mockUser);
+
+        if (bookmark.isEmpty()) {
+            bookmarkRepository.save(new Bookmark(mockUser, article));
+        } else {
+            bookmarkRepository.delete(bookmark.get());
+        }
+    }
+
+    public DetailPortfolioDto getDetailPortfolio(Long articleId) {
+        /* 토큰을 통한 user 객체를 불러옴 -> 현재 임시 객체*/
+        User mockUser = userRepository.findById(1L).orElseThrow(UserNotFoundException::new);
+        Article article = articleRepository.findById(articleId).orElseThrow(ArticleNotFoundException::new);
+
+        article.plusViewCount(article.getViewCount() + 1);
+        articleRepository.save(article);
+
+        int heartCount = articleLikeRepository.countByArticle(article);
+        int bookmarkCount = bookmarkRepository.countByArticle(article);
+
+        List<Article> portfolioArticlesByUserId = articleRepository.findPortfolioArticlesByUserId(mockUser.getUserId());
+
+        return new DetailPortfolioDto(article, mockUser, heartCount, bookmarkCount, portfolioArticlesByUserId);
+    }
 }
