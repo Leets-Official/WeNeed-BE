5c3877604e0d34edf7635c203bfa6a79c05268b4
노정완 <jeongwan.noh@gmail.com>
2024-01-18 21:05:20 +0900

    :art: [fix]: update 쿼리문이 두번 작동할 수 있는 이슈를 수정

--- src/main/java/org/example/weneedbe/global/config/jwt/service/RefreshTokenService.java
+++ src/main/java/org/example/weneedbe/global/config/jwt/service/RefreshTokenService.java
@@ -6,6 +6,8 @@ import org.example.weneedbe.domain.token.repository.RefreshTokenRepository;
 import org.example.weneedbe.global.config.jwt.exception.InvalidTokenException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class RefreshTokenService {
@@ -17,10 +19,14 @@ public class RefreshTokenService {
     }
 
     public void saveRefreshToken(Long userId, String newRefreshToken) {
-        RefreshToken refreshToken = refreshTokenRepository.findByUserId(userId)
-                .map(entity -> entity.update(newRefreshToken))
-                .orElse(new RefreshToken(userId, newRefreshToken));
+        Optional<RefreshToken> optionalRefreshToken = refreshTokenRepository.findByUserId(userId);
 
-        refreshTokenRepository.save(refreshToken);
+        if (optionalRefreshToken.isPresent()) {
+            RefreshToken existingRefreshToken = optionalRefreshToken.get();
+            existingRefreshToken.update(newRefreshToken);
+            return;
+        }
+        RefreshToken initialRefreshToken = new RefreshToken(userId, newRefreshToken);
+        refreshTokenRepository.save(initialRefreshToken);
     }
 }
