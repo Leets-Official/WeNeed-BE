9ba76b35e9e6c3a037b277a4b33b62d9023e4c61
jiixon <jr0107380@gmail.com>
2024-01-19 00:34:39 +0900

    :sparkles:[feat]: security에 ExceptionHandleFilter 추가

--- src/main/java/org/example/weneedbe/global/config/security/SecurityConfig.java
+++ src/main/java/org/example/weneedbe/global/config/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.example.weneedbe.global.config.security;
 import lombok.RequiredArgsConstructor;
 import org.example.weneedbe.global.config.jwt.TokenAuthenticationFilter;
 import org.example.weneedbe.global.config.jwt.TokenProvider;
+import org.example.weneedbe.global.filter.ExceptionHandleFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -40,6 +41,7 @@ public class SecurityConfig {
             )
             .addFilterBefore(new TokenAuthenticationFilter(tokenProvider),
                     UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(new ExceptionHandleFilter(), TokenAuthenticationFilter.class)
             .authorizeHttpRequests((authorize) ->
                     authorize
                             .requestMatchers("/").permitAll()
