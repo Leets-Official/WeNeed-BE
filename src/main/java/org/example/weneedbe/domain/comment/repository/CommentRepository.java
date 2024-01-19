4978ba066f9a449bf0ea9f971346036ff558855b
jiixon <jr0107380@gmail.com>
2024-01-19 21:31:47 +0900

    :sparkles:[feat]: comment 가져오는 jpa 구현

--- src/main/java/org/example/weneedbe/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/weneedbe/domain/comment/repository/CommentRepository.java
@@ -1,8 +1,12 @@
 package org.example.weneedbe.domain.comment.repository;
 
+import org.example.weneedbe.domain.article.domain.Article;
 import org.example.weneedbe.domain.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Optional<Comment> findAllByArticle(Article article);
 
 }
