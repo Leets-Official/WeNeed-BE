fc359d87f095d5c447a1af2b8304eb71e658c01e
jiixon <jr0107380@gmail.com>
2024-01-20 03:00:11 +0900

    :sparkles:[feat]: 게시물 댓글 리스트 조회 jpa 구현

--- src/main/java/org/example/weneedbe/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/weneedbe/domain/comment/repository/CommentRepository.java
@@ -4,9 +4,8 @@ import org.example.weneedbe.domain.article.domain.Article;
 import org.example.weneedbe.domain.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    Optional<Comment> findAllByArticle(Article article);
-
+    List<Comment> findAllByArticle(Article article);
 }
