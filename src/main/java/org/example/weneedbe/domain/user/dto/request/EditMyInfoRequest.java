3c10ac754779ff090dcba63567cee8962a22d616
노정완 <jeongwan.noh@gmail.com>
2024-01-21 00:58:15 +0900

    :sparkles: [feat]: 프로필 수정 request dto 생성

--- src/main/java/org/example/weneedbe/domain/user/dto/request/EditMyInfoRequest.java
+++ src/main/java/org/example/weneedbe/domain/user/dto/request/EditMyInfoRequest.java
@@ -4,11 +4,16 @@ import lombok.Data;
 import org.example.weneedbe.domain.user.domain.Department;
 import org.example.weneedbe.domain.user.domain.Fields;
 
+import java.util.List;
+
 @Data
-public class UserInfoRequest {
+public class EditMyInfoRequest {
+    private String profile;
     private String nickname;
-    private Integer userGrade;
     private Department major;
+    private Integer userGrade;
     private Department doubleMajor;
     private Fields interestField;
+    private List<String> links;
+    private String selfIntro;
 }
