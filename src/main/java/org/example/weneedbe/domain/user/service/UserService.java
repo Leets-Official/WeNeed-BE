3c1834ec5a071989894dcdef49df191bd1f4ec28
anxi01 <tjdals2893@naver.com>
2024-01-21 19:05:10 +0900

    :sparkles: [feat]: 관심 크루 조회 service 추가

--- src/main/java/org/example/weneedbe/domain/user/service/UserService.java
+++ src/main/java/org/example/weneedbe/domain/user/service/UserService.java
@@ -1,11 +1,19 @@
 package org.example.weneedbe.domain.user.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.weneedbe.domain.article.domain.Article;
+import org.example.weneedbe.domain.article.domain.Type;
+import org.example.weneedbe.domain.article.repository.ArticleLikeRepository;
+import org.example.weneedbe.domain.bookmark.domain.Bookmark;
+import org.example.weneedbe.domain.bookmark.repository.BookmarkRepository;
 import org.example.weneedbe.domain.user.domain.User;
 import org.example.weneedbe.domain.user.dto.request.UserInfoRequest;
 import org.example.weneedbe.domain.user.dto.response.UserInfoResponse;
+import org.example.weneedbe.domain.user.dto.response.mypage.MyPageArticleInfoResponse;
 import org.example.weneedbe.domain.user.dto.response.mypage.MyPageGetMyInfoResponse;
+import org.example.weneedbe.domain.user.exception.UserNotFoundException;
 import org.example.weneedbe.domain.user.repository.UserRepository;
 import org.example.weneedbe.global.jwt.TokenProvider;
 import org.springframework.http.HttpStatus;
@@ -19,6 +27,8 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final TokenProvider tokenProvider;
+    private final BookmarkRepository bookmarkRepository;
+    private final ArticleLikeRepository articleLikeRepository;
 
     public Boolean checkNicknameDuplicate(String nickName) {
         return userRepository.existsByNickname(nickName);
@@ -66,5 +76,21 @@ public class UserService {
 
         return MyPageGetMyInfoResponse.from(mockUser);
 
+    }
+
+    public List<MyPageArticleInfoResponse> getInterestingCrewInfo(String authorizationHeader) {
+        Long userId = getUserIdFromHeader(authorizationHeader);
+        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+
+        List<Bookmark> recruitingBookmarks = bookmarkRepository.findAllByUserAndArticle_ArticleTypeOrderByArticle_CreatedAtDesc(
+            user, Type.RECRUITING);
+
+        return recruitingBookmarks.stream().map(s -> new MyPageArticleInfoResponse(s,
+            articleLikeRepository.countByArticle(s.getArticle()))).toList();
+    }
+
+    private Long getUserIdFromHeader(String authorizationHeader) {
+        String token = tokenProvider.getTokenFromAuthorizationHeader(authorizationHeader);
+        return tokenProvider.getUserIdFromToken(token);
     }
 }
