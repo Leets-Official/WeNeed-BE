b872be4afebe2cf3b8c176f08fb9c4b592a448da
jiixon <jr0107380@gmail.com>
2024-01-23 04:57:36 +0900

    :sparkles:[feat]: 리크루팅조회 jpa 쿼리 수정

--- src/main/java/org/example/weneedbe/domain/article/repository/ArticleRepository.java
+++ src/main/java/org/example/weneedbe/domain/article/repository/ArticleRepository.java
@@ -44,8 +44,8 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
     @Query("SELECT ar FROM Article ar WHERE ar.articleType = 'PORTFOLIO'")
     List<Article> findPortfolios();
 
-    @Query(value = "SELECT a.* FROM article a INNER JOIN detail_tags dt ON a.article_id = dt.article_id WHERE a.article_type = 'RECRUITING' AND (:detailTags IS NULL OR dt.detail_tags IN :detailTags) ORDER BY a.created_at DESC",
-            countQuery = "SELECT count(*) FROM article a INNER JOIN detail_tags dt ON a.article_id = dt.article_id WHERE a.article_type = 'RECRUITING' AND (:detailTags IS NULL OR dt.detail_tags IN :detailTags)",
+    @Query(value = "SELECT a.* FROM article a LEFT JOIN detail_tags dt ON a.article_id = dt.article_id AND a.article_type = 'RECRUITING' AND (:detailTags IS NULL OR dt.detail_tags IN :detailTags) ORDER BY a.created_at DESC",
+            countQuery = "SELECT count(*) FROM article a LEFT JOIN detail_tags dt ON a.article_id = dt.article_id AND a.article_type = 'RECRUITING' AND (:detailTags IS NULL OR dt.detail_tags IN :detailTags)",
             nativeQuery = true)
     Page<Article> findRecruitingByDetailTagsInOrderByCreatedAtDesc(@Param("detailTags") String[] detailTags, Pageable pageable);
 
