9fd97daef2209d62ccdfd2461faa62d0fcaf2bbc
노정완 <jeongwan.noh@gmail.com>
2024-01-24 01:44:09 +0900

    :sparkles: [feat]: 사용자가 작성한 포트폴리오 게시글 가져오는 쿼리 로직 작성

--- src/main/java/org/example/weneedbe/domain/user/repository/UserArticleRepository.java
+++ src/main/java/org/example/weneedbe/domain/user/repository/UserArticleRepository.java
@@ -1,2 +1,14 @@
-package org.example.weneedbe.domain.user.repository;public interface UserArticleRepository {
+package org.example.weneedbe.domain.user.repository;
+
+import org.example.weneedbe.domain.article.domain.Type;
+import org.example.weneedbe.domain.user.domain.User;
+import org.example.weneedbe.domain.user.domain.UserArticle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserArticleRepository extends JpaRepository<UserArticle, Long> {
+
+    List<UserArticle> findAllByUserAndArticle_ArticleTypeOrderByArticle_CreatedAtDesc(User user, Type articleType);
+
 }
