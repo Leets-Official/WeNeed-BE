d1619eebac9d04ef3d1e870eefa150e57547d866
anxi01 <tjdals2893@naver.com>
2024-01-24 14:29:39 +0900

    :sparkles: [feat]: 작성자와 사용자 불일치시의 예외 코드 추가

--- src/main/java/org/example/weneedbe/global/error/ErrorCode.java
+++ src/main/java/org/example/weneedbe/global/error/ErrorCode.java
@@ -17,7 +17,8 @@ public enum ErrorCode {
     INVALID_SORT_ERROR(400, "INVALID_SORT_ERROR", "잘못된 정렬 값입니다."),
     INVALID_INPUT_VALUE(400, "INVALID_INPUT_VALUE", "유효하지 않은 입력값입니다."),
     INVALID_EDIT_VALUE(400, "INVALID_EDIT_VALUE", "유효하지 않은 변경값입니다."),
-    USER_NOT_REGISTERED(400, "USER_NOT_REGISTERED","회원가입이 완료되지 않은 유저입니다.");
+    USER_NOT_REGISTERED(400, "USER_NOT_REGISTERED","회원가입이 완료되지 않은 유저입니다."),
+    AUTHOR_MISMATCH_ERROR(400, "AUTHOR_MISMATCH_ERROR", "작성자와 사용자가 일치하지 않습니다.");
 
     private final int httpStatus;
     private final String code;
