36559d13b06c95d1a717666fecb3f76082f1aeaa
노정완 <jeongwan.noh@gmail.com>
2024-01-24 18:03:59 +0900

    :sparkles: [feat]: 동일한 articleId를 기준으로 List<Article>을 반환하는 쿼리 로직 구현

--- src/main/java/org/example/weneedbe/domain/user/repository/UserArticleRepository.java
+++ src/main/java/org/example/weneedbe/domain/user/repository/UserArticleRepository.java
@@ -4,11 +4,11 @@ import org.example.weneedbe.domain.article.domain.Type;
 import org.example.weneedbe.domain.user.domain.User;
 import org.example.weneedbe.domain.user.domain.UserArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface UserArticleRepository extends JpaRepository<UserArticle, Long> {
 
     List<UserArticle> findAllByUserAndArticle_ArticleTypeOrderByArticle_CreatedAtDesc(User user, Type articleType);
 
+    List<UserArticle> findAllByArticle_ArticleId(Long articleId);
 }
