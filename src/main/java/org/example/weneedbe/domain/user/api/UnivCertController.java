63c2e9a57b1400b5acede7fe1a2a4e44e5967115
노정완 <jeongwan.noh@gmail.com>
2024-01-25 16:53:46 +0900

    :recycle: [refactor]: 메일 인증 관련 api 로직 수정

--- src/main/java/org/example/weneedbe/domain/user/api/UnivCertController.java
+++ src/main/java/org/example/weneedbe/domain/user/api/UnivCertController.java
@@ -7,27 +7,22 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
+import org.example.weneedbe.domain.user.dto.request.CertifyCodeRequest;
 import org.example.weneedbe.global.error.ErrorResponse;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
 @Tag(name = "Mail Certification Controller", description = "메일 인증 관련 API입니다.")
 @RestController
 @RequestMapping("/api/v1")
-@Transactional
 @Slf4j
 public class UnivCertController {
     private final static String UNIV_NAME = "가천대학교";
     @Value("${univcertKey}")
     private String apiKey;
-    private String email;
 
     @Operation(summary = "학교 메일 조회", description = "입력한 이메일이 가천대학교 소속 이메일인지 확인합니다.")
     @ApiResponses({
@@ -38,8 +33,7 @@ public class UnivCertController {
     })
     @PostMapping("/certify")
     public void checkMailWithUniv(@RequestParam String email) throws IOException {
-        this.email = email;
-        log.info("\nemail:{}", this.email);
+        log.info("\nemail:{}", email);
         UnivCert.certify(apiKey, email, UNIV_NAME, true);
     }
 
@@ -51,7 +45,7 @@ public class UnivCertController {
             @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @PostMapping("/certifycode")
-    public void checkVerificationCode(@RequestParam int code) throws IOException {
-        UnivCert.certifyCode(apiKey, email, UNIV_NAME, code);
+    public void checkVerificationCode(@RequestBody CertifyCodeRequest request) throws IOException {
+        UnivCert.certifyCode(apiKey, request.getEmail(), UNIV_NAME, request.getCode());
     }
 }
