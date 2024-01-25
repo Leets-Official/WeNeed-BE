dbee0253b2f9efa58c0fa6c8d25a1fdb1c8a0cff
jiixon <jr0107380@gmail.com>
2024-01-25 17:39:35 +0900

    :sparkles:[feat]: 게시물 삭제 service 구현 및 메서드분리

--- src/main/java/org/example/weneedbe/domain/article/application/ArticleService.java
+++ src/main/java/org/example/weneedbe/domain/article/application/ArticleService.java
@@ -123,9 +123,8 @@ public class ArticleService {
     }
 
     public DetailPortfolioDto getDetailPortfolio(String authorizationHeader, Long articleId) {
-        Long userId = getUserIdFromAuthorizationHeader(authorizationHeader);
-        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
-        Article article = articleRepository.findById(articleId).orElseThrow(ArticleNotFoundException::new);
+        User user = findUser(authorizationHeader);
+        Article article = findArticle(articleId);
 
         article.plusViewCount(article.getViewCount() + 1);
         articleRepository.save(article);
@@ -156,16 +155,9 @@ public class ArticleService {
         return workList;
     }
 
-    private Long getUserIdFromAuthorizationHeader(String authorizationHeader) {
-        String token = tokenProvider.getTokenFromAuthorizationHeader(authorizationHeader);
-        Long userIdFromToken = tokenProvider.getUserIdFromToken(token);
-        return userIdFromToken;
-    }
-
     public DetailRecruitDto getDetailRecruit(String authorizationHeader, Long articleId) {
-        Long userId = getUserIdFromAuthorizationHeader(authorizationHeader);
-        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
-        Article article = articleRepository.findById(articleId).orElseThrow(ArticleNotFoundException::new);
+        User user = findUser(authorizationHeader);
+        Article article = findArticle(articleId);
 
         article.plusViewCount(article.getViewCount() + 1);
         articleRepository.save(article);
@@ -193,15 +185,10 @@ public class ArticleService {
     public void editPortfolio(String authorizationHeader, Long articleId, MultipartFile thumbnail,
         List<MultipartFile> images, List<MultipartFile> files, ArticleRequest request)
         throws IOException {
+        User user = findUser(authorizationHeader);
+        Article article = findArticle(articleId);
 
-        Long userId = getUserIdFromAuthorizationHeader(authorizationHeader);
-        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
-        Article article = articleRepository.findById(articleId)
-            .orElseThrow(ArticleNotFoundException::new);
-
-        if (!user.equals(article.getUser())) {
-            throw new AuthorMismatchException();
-        }
+        validateUserOwnership(article, user);
 
         /* 기존 데이터 삭제 */
         userArticleRepository.deleteAllByArticle_ArticleId(articleId);
@@ -232,5 +219,30 @@ public class ArticleService {
             );
         }
         return userArticles;
+    }
+    public void deleteArticle(String authorizationHeader, Long articleId){
+        User user = findUser(authorizationHeader);
+        Article article = findArticle(articleId);
+
+        validateUserOwnership(article, user);
+
+        articleRepository.delete(article);
+    }
+
+    private User findUser(String authorizationHeader){
+        String token = tokenProvider.getTokenFromAuthorizationHeader(authorizationHeader);
+        Long userId = tokenProvider.getUserIdFromToken(token);
+        return userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+    }
+
+    private Article findArticle(Long articleId){
+        return articleRepository.findById(articleId)
+                .orElseThrow(ArticleNotFoundException::new);
+    }
+
+    private void validateUserOwnership(Article article, User user){
+        if (!user.equals(article.getUser())) {
+            throw new AuthorMismatchException();
+        }
     }
 }
