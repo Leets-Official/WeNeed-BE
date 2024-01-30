e4095e9ed7b563ab70b4b26bb1970b368ce6d7d8
anxi01 <tjdals2893@naver.com>
2024-01-30 23:03:50 +0900

    :recycle: [refactor]: 식별자 id String으로 변경

--- src/main/java/org/example/weneedbe/domain/article/domain/ContentData.java
+++ src/main/java/org/example/weneedbe/domain/article/domain/ContentData.java
@@ -1,7 +1,6 @@
 package org.example.weneedbe.domain.article.domain;
 
 import jakarta.persistence.Embeddable;
-import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,7 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ContentData {
-  private UUID id = UUID.randomUUID();
+  private String id;
   private String type;
   private String data;
 }
