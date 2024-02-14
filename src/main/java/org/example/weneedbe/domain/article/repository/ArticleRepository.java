5b55640e9cacb478147e1fea343066d81263e86f
jiixon <jr0107380@gmail.com>
2024-02-14 21:39:58 +0900

    :recycle:[refactor]: articleRepository 쿼리메소드 수정

--- src/main/java/org/example/weneedbe/domain/article/repository/ArticleRepository.java
+++ src/main/java/org/example/weneedbe/domain/article/repository/ArticleRepository.java
@@ -14,8 +14,8 @@ import java.util.List;
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
-    @Query(value = "SELECT a.* FROM article a LEFT JOIN detail_tags dt ON a.article_id = dt.article_id AND a.article_type = 'PORTFOLIO' AND (:detailTags IS NULL OR dt.detail_tags IN :detailTags) ORDER BY a.created_at DESC",
-            countQuery = "SELECT count(*) FROM article a LEFT JOIN detail_tags dt ON a.article_id = dt.article_id AND a.article_type = 'PORTFOLIO' AND (:detailTags IS NULL OR dt.detail_tags IN :detailTags)",
+    @Query(value = "SELECT a.* FROM article a LEFT JOIN detail_tags dt ON a.article_id = dt.article_id WHERE a.article_type = 'PORTFOLIO' AND (:detailTags IS NULL OR dt.detail_tags IN :detailTags) ORDER BY a.created_at DESC",
+            countQuery = "SELECT count(*) FROM article a LEFT JOIN detail_tags dt ON a.article_id = dt.article_id WHERE a.article_type = 'PORTFOLIO' AND (:detailTags IS NULL OR dt.detail_tags IN :detailTags)",
             nativeQuery = true)
     Page<Article> findPortfoliosByDetailTagsInOrderByCreatedAtDesc(@Param("detailTags") String[] detailTags, Pageable pageable);
 
@@ -45,8 +45,8 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
     @Query("SELECT ar FROM Article ar WHERE ar.articleType = 'PORTFOLIO'")
     List<Article> findPortfolios();
 
-    @Query(value = "SELECT a.* FROM article a LEFT JOIN detail_tags dt ON a.article_id = dt.article_id AND a.article_type = 'RECRUITING' AND (:detailTags IS NULL OR dt.detail_tags IN :detailTags) ORDER BY a.created_at DESC",
-            countQuery = "SELECT count(*) FROM article a LEFT JOIN detail_tags dt ON a.article_id = dt.article_id AND a.article_type = 'RECRUITING' AND (:detailTags IS NULL OR dt.detail_tags IN :detailTags)",
+    @Query(value = "SELECT a.* FROM article a LEFT JOIN detail_tags dt ON a.article_id = dt.article_id WHERE a.article_type = 'RECRUITING' AND (:detailTags IS NULL OR dt.detail_tags IN :detailTags) ORDER BY a.created_at DESC",
+            countQuery = "SELECT count(*) FROM article a LEFT JOIN detail_tags dt ON a.article_id = dt.article_id WHERE a.article_type = 'RECRUITING' AND (:detailTags IS NULL OR dt.detail_tags IN :detailTags)",
             nativeQuery = true)
     Page<Article> findRecruitingByDetailTagsInOrderByCreatedAtDesc(@Param("detailTags") String[] detailTags, Pageable pageable);
 
