1211bdca6c36c23a9e9dfb8a274adaf5c5e3639c
anxi01 <tjdals2893@naver.com>
2024-02-28 23:46:33 +0900

    :sparkles: [feat]: 게시물 지원서 조회 로직 생성

--- src/main/java/org/example/weneedbe/domain/application/application/ApplicationService.java
+++ src/main/java/org/example/weneedbe/domain/application/application/ApplicationService.java
@@ -3,10 +3,11 @@ package org.example.weneedbe.domain.application.application;
 import lombok.RequiredArgsConstructor;
 import org.example.weneedbe.domain.application.domain.Recruit;
 import org.example.weneedbe.domain.application.dto.request.RecruitFormRequest;
+import org.example.weneedbe.domain.application.dto.response.RecruitFormResponse;
 import org.example.weneedbe.domain.application.repository.RecruitRepository;
+import org.example.weneedbe.domain.article.application.ArticleService;
 import org.example.weneedbe.domain.article.domain.Article;
-import org.example.weneedbe.domain.article.exception.ArticleNotFoundException;
-import org.example.weneedbe.domain.article.repository.ArticleRepository;
+import org.example.weneedbe.domain.bookmark.service.BookmarkService;
 import org.example.weneedbe.domain.user.domain.User;
 import org.example.weneedbe.domain.user.service.UserService;
 import org.springframework.stereotype.Service;
@@ -16,16 +17,30 @@ import org.springframework.stereotype.Service;
 public class ApplicationService {
 
   private final RecruitRepository recruitRepository;
-  private final ArticleRepository articleRepository;
   private final UserService userService;
+  private final ArticleService articleService;
+  private final BookmarkService bookmarkService;
 
   public void createRecruitForm(String authorizationHeader, Long articleId, RecruitFormRequest request) {
 
-    Article article = articleRepository.findById(articleId).orElseThrow(ArticleNotFoundException::new);
+    Article article = articleService.findArticle(articleId);
     User user = userService.findUser(authorizationHeader);
 
     Recruit recruit = Recruit.of(article, user, request);
 
     recruitRepository.save(recruit);
+  }
+
+  public RecruitFormResponse getRecruitForm(String authorizationHeader, Long articleId) {
+
+    User user = userService.findUser(authorizationHeader);
+
+    Article article = articleService.findArticle(articleId);
+    int heartCount = articleService.countHeartByArticle(article);
+    int bookmarkCount = bookmarkService.countBookmarkByArticle(article);
+
+    Recruit recruit = recruitRepository.findByArticle_ArticleId(articleId).orElseThrow();
+
+    return new RecruitFormResponse(user, article, heartCount, bookmarkCount, recruit);
   }
 }
