6074af20876c699c1c661eed5dfbc1e585c0aeee
jiixon <jr0107380@gmail.com>
2024-03-01 03:23:41 +0900

    :sparkles:[feat]: applicationform request dto 작성

--- src/main/java/org/example/weneedbe/domain/application/dto/request/ApplicationFormRequest.java
+++ src/main/java/org/example/weneedbe/domain/application/dto/request/ApplicationFormRequest.java
@@ -1,2 +1,21 @@
-package org.example.weneedbe.domain.application.dto.request;public class ApplicationFormRequest {
+package org.example.weneedbe.domain.application.dto.request;
+
+import lombok.Getter;
+import java.util.List;
+
+@Getter
+public class ApplicationFormRequest {
+
+    private String name;
+    private String major;
+    private String doubleMajor;
+    private boolean international;
+    private Integer grade;
+    private String status;
+    private String phone;
+    private String aboutMe;
+    private String content;
+    private List<String> keywords;
+    private List<String> crewAnswers;
+
 }
