3cfbd00a7f8cb8d1cac567b52e800495baac582e
anxi01 <tjdals2893@naver.com>
2024-03-12 00:41:19 +0900

    :sparkles: [feat]: articleId로 파일 조회하는 메소드 추가

--- src/main/java/org/example/weneedbe/domain/file/repository/FileRepository.java
+++ src/main/java/org/example/weneedbe/domain/file/repository/FileRepository.java
@@ -1,9 +1,12 @@
 package org.example.weneedbe.domain.file.repository;
 
+import java.util.List;
 import org.example.weneedbe.domain.file.domain.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FileRepository extends JpaRepository<File, Long> {
 
   void deleteAllByArticle_ArticleId(Long article_articleId);
+
+  List<File> findAllByArticle_ArticleId(Long article_articleId);
 }
